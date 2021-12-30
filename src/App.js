ae7bdbe394815344c9830ab47b0fc676a7426bd8
Arjun Ramesh <aramesh@MacBook-Pro.local>
2021-12-30 23:07:45 +0530

    Initial version from tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Home from './pages/Home';
+import SearchPage from './pages/SearchPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+
+          {/* SearchPage (the list of search results) */}
+          <Route path="/search" element={<SearchPage />}>
+          </Route>
+         
+
+          {/* Home (the page with the search) */}
+          <Route path="/" element={<Home />}>
+          </Route>
+
+        </Routes>
+      </Router>
     </div>
   );
 }
