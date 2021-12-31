bcab372556f9da7b02a41def03292a799dc04b1c
Arjun Ramesh <aramesh@MacBook-Pro.local>
2021-12-31 13:27:58 +0530

    Dont open in a browser by default, make api calls through an express backend

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -1,15 +1,12 @@
 import { useState, useEffect } from 'react';
 
-const CONTEXT_KEY=process.env.REACT_APP_BROWSER_CONTEXT_KEY; 
-const API_KEY=process.env.REACT_APP_SEARCH_API_KEY;
-
 const useGoogleSearch = (term) => {
     const [data, setData] = useState(null);
     
     useEffect(() => {
         const fetchData = async() => {
             fetch(
-                `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+                `http://localhost:8000/search/?term=${term}`
             )
             .then(response => response.json())
             .then(result => {
